09b34bb64a408ca4af7c8230fff3d49ac5cc1dc2
Milica Lalic <milica.lalic2358@gmail.com>
2025-12-12 17:42:15 +0100

    Add GitHub Pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+const repo = "my-blog";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://mlalic2358.github.io",
+  base: import.meta.env.PROD ? `/${repo}/` : "/",
 });
