17806ae91fdb49d0e03544a9fd2a9ef05347f130
Milica Lalic <milica.lalic2358@gmail.com>
2025-12-14 01:03:10 +0100

    Add books section

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,4 +16,16 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const books = defineCollection({
+	loader: glob({ base: './src/content/books', pattern: '**/*.{md,mdx}' }),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
+});
+
+export const collections = { blog, books };
---
+++ src/content/books/book-example.md
@@ -0,0 +1,8 @@
+---
+title: 'Example Book'
+description: 'This is just an example book.'
+pubDate: 'Dec 13 2025'
+heroImage: '../../assets/blog-placeholder-5.jpg'
+---
+
+# Interesting remarks...
